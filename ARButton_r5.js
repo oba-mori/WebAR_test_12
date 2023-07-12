87251fa79924b4fae48411c48cdab17bbdfd9042
oba-mori <99237421+oba-mori@users.noreply.github.com>
2023-07-12 10:16:05 +0900

    Add files via upload

--- ARButton_r5.js
+++ ARButton_r5.js
@@ -70,6 +70,17 @@ class ARButton {
 
 
 
+				// ボタンクリックイベント
+				button_test1.addEventListener('click', () => {
+					//変数値を変更する関数
+					this.incrementValue();
+
+					//表示
+					console.log('ボタンクリック後の値:', this.value);
+
+					// 値の変更を検知し、登録されたコール関数に通知するためのもの
+					this.dispatchValueChangedEvent();
+				});
 
 
 
@@ -266,10 +277,12 @@ class ARButton {
 		if (this.value == 0)
 		{
   	  this.value = 1;
+			console.log("true this.value : ", this.value);
 		}
 		else
 		{
 			this.value = 0;
+			console.log("else this.value : ", this.value);
 		}
 	}
 
